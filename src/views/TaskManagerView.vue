91ed2b97776d873d9c9487e0d6b0f32d742017da
TheSenya <senyagithub@gmail.com>
2025-03-16 18:19:01 -0400

    add taskmanager page name

--- src/views/TaskManagerView.vue
+++ src/views/TaskManagerView.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="notes-container">
-        <h1>Notes</h1>
+        <h1>Task Manager</h1>
         <!-- Add your notes content here -->
     </div>
 </template>
+
+<script>
+export default {
+    name: 'TaskManagerView',
+}
+</script>
