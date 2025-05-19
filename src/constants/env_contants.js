ee51f1a05adcb615ae09561608580c0e5ade96aa
TheSenya <145071510+TheSenya@users.noreply.github.com>
2025-05-19 13:49:53 -0400

    dev to main (#4)
    * docker build update
    
    * add nginx config
    
    * added back woirkflow
    
    * update prod constants

--- src/constants/env_contants.js
+++ src/constants/env_contants.js
@@ -1,5 +1,5 @@
 const envVariables = {
-    API_BASE_URL: import.meta.env.PROD
+    API_BASE_URL: import.meta.env.VITE_PROD === 'true'
         ? import.meta.env.VITE_API_BASE_URL_PROD
         : import.meta.env.VITE_API_BASE_URL_DEV || '/api/v1',
 }
