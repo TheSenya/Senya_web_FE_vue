7c139fd0726960aa94d2c55e78ebc2c55603223f
TheSenya <145071510+TheSenya@users.noreply.github.com>
2025-05-19 14:21:02 -0400

    dev to main (#6)
    * docker build update
    
    * add nginx config
    
    * added back woirkflow
    
    * update prod constants
    
    * update to env constants
    
    * constant update

--- src/constants/env_contants.js
+++ src/constants/env_contants.js
@@ -1,20 +1,21 @@
 console.log('Vite Mode:', import.meta.env.MODE);
 console.log('Vite PROD flag:', import.meta.env.PROD);
-console.log('VITE_API_BASE_URL_PROD from env:', import.meta.env.VITE_API_BASE_URL_PROD);
-console.log('VITE_API_BASE_URL_DEV from env:', import.meta.env.VITE_API_BASE_URL_DEV);
+console.log('API_BASE_URL_PROD from env:', import.meta.env.API_BASE_URL_PROD);
+console.log('API_BASE_URL_DEV from env:', import.meta.env.API_BASE_URL_DEV);
 
 
 export const envVariables = {
-  API_BASE_URL: import.meta.env.VITE_MODE === 'production'
-      ? import.meta.env.VITE_API_BASE_URL_PROD
-      : import.meta.env.VITE_API_BASE_URL_DEV || '/api/v1',
+  API_BASE_URL: import.meta.env.MODE === 'production'
+      ? import.meta.env.API_BASE_URL_PROD
+      : import.meta.env.API_BASE_URL_DEV || '/api/v1',
+
 };
 
 console.log('Calculated API_BASE_URL:', envVariables.API_BASE_URL);
 
 // Add a check for undefined specifically for production
-if (import.meta.env.PROD && typeof import.meta.env.VITE_API_BASE_URL_PROD === 'undefined') {
-    console.error("CRITICAL: VITE_API_BASE_URL_PROD is undefined in production build!");
+if (import.meta.env.PROD && typeof import.meta.env.API_BASE_URL_PROD === 'undefined') {
+    console.error("CRITICAL: API_BASE_URL_PROD is undefined in production build!");
 }
 if (import.meta.env.PROD && typeof envVariables.API_BASE_URL === 'undefined') {
     console.error("CRITICAL: envVariables.API_BASE_URL resolved to undefined in production build!");
