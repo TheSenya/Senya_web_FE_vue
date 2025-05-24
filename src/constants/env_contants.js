db2ae15a33fd5dac6d66453d5e2565e0c9d36e03
SenyaGithub <senyagithub@gmail.com>
2025-05-23 20:10:12 -0400

    update env constanss

--- src/constants/env_contants.js
+++ src/constants/env_contants.js
@@ -1,7 +1,7 @@
 const envVariables = {
     API_BASE_URL: import.meta.env.PROD
-        ? import.meta.env.VITE_API_BASE_URL_PROD
-        : import.meta.env.VITE_API_BASE_URL_DEV || '/api/v1',
+        ? "https://api.senya.ca/api/v1"
+        : "http://localhost:5173/api/v1"
 }
 
 export default envVariables
